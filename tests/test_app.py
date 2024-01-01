843cab4f257b20c7dc48d192251bfed6c3b4aec2
M. Quasar <marcosm@gmail.com>
2024-01-01 19:57:15 -0300

    Configuração inicial do projeto

---
+++ tests/test_app.py
@@ -0,0 +1,23 @@
+def test_root_deve_retornar_200_e_ola_mundo(client):
+    response = client.get('/')
+
+    assert response.status_code == 200
+    assert response.json() == {'message': 'Olá mundo!'}
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'alice',
+            'email': 'alice@example.com',
+            'password': 'secret',
+        },
+    )
+
+    assert response.status_code == 201
+    assert response.json() == {
+        'username': 'alice',
+        'email': 'alice@example.com',
+        'id': 1,
+    }
